dc5b4c55e7b6a297293600f1957a7a8405549790
unknown <you@example.com>
2024-05-05 02:05:39 +0700

    Update Main

--- Main.java
+++ Main.java
@@ -3,12 +3,10 @@ public class Main {
         System.out.println("---- Book Information ----");
         
         TextBook Book1 = new TextBook("Analysis of Genes and Genomes", "Richard Reece", 2006, "Biology");
-        Book1.displayInfo();
-        
         Novel Book2 = new Novel("Frankenstein", "Mary Shelley", 1818, "Sci-fi, Horror");
-        Book2.displayInfo();
-        
         Magazine Book3  = new Magazine("The New Yorker", "Harold W. Ross", 1925, "Politics");
+        Book1.displayInfo();
+        Book2.displayInfo();
         Book3.displayInfo();
         
         System.out.println("---- Borrowing and Returning Books ----");
